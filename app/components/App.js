ab9267cb8a3599865aa7e82dd628aaf444a5c0dc
EdwardGoomba <ask@edwardvdesign.com>
2018-11-14 16:36:59 -0500

    Create nav, add router, update styles

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 
+import Nav from './Nav'
 import Popular from './Popular'
+
+const ReactRouter = require('react-router-dom')
+const Router = ReactRouter.BrowserRouter
+const Route = ReactRouter.Route
 
 class App extends Component {
   render () {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
+
     )
   }
 }
