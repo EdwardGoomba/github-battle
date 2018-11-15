5274a8c5f7ae9f4bfe8c340542800cb409f02fcf
EdwardGoomba <ask@edwardvdesign.com>
2018-11-14 23:42:54 -0500

    Built Battle and Home component, setup routes

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
+import Home from './Home'
 import Nav from './Nav'
+import Battle from './Battle'
 import Popular from './Popular'
 
 const ReactRouter = require('react-router-dom')
 const Router = ReactRouter.BrowserRouter
 const Route = ReactRouter.Route
+const Switch = ReactRouter.Switch
 
 class App extends Component {
   render () {
@@ -13,7 +16,14 @@ class App extends Component {
       <Router>
         <div className='container'>
           <Nav />
-          <Route path='/popular' component={Popular} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function () {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
         </div>
       </Router>
 
