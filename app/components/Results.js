07929be1d26885d675a415be557e0388b53a8eb4
EdwardGoomba <ask@edwardvdesign.com>
2019-04-16 15:52:51 -0400

    Refactor results component

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react'
 import queryString from 'query-string'
 import { Link } from 'react-router-dom'
 import api from '../../utils/api'
-var PropTypes = require('prop-types')
+import PropTypes from 'prop-types'
 import PlayerPreview from './PlayerPreview'
 import Loading from './Loading'
 
-function Profile(props) {
-  var info = props.info;
+function Profile({ info }) {
   return (
     <PlayerPreview
-      avatar={info.avatar_url}
-      username={info.login}
+      avatar={avatar_url}
+      username={login}
     >
       <ul className='space-list-items'>
         {info.name && <li>{info.name}</li>}
@@ -30,12 +29,12 @@ Profile.propTypes = {
   info: PropTypes.object.isRequired,
 }
 
-function Player(props) {
+function Player({ label, score, profile }) {
   return (
     <div>
-      <h1 className='header'>{props.label}</h1>
-      <h3 style={{ textAlign: 'center' }}>Score: {props.score}</h3>
-      <Profile info={props.profile} />
+      <h1 className='header'>{label}</h1>
+      <h3 style={{ textAlign: 'center' }}>Score: {score}</h3>
+      <Profile info={profile} />
     </div>
   )
 }
@@ -59,37 +58,32 @@ class Results extends Component {
 
 
   componentDidMount() {
-    var players = queryString.parse(this.props.location.search)
+    const { playerOneName, playerOneName } = queryString.parse(this.props.location.search)
 
     api.battle([
-      players.playerOneName,
-      players.playerTwoName
-    ]).then(function (results) {
+      playerOneName,
+      playerOneName
+    ]).then((results) => {
       if (results === null) {
-        return this.setState(function () {
-          return {
-            error: 'Looks like there was an error. Check that both users exist on Github',
-            loading: false
-          }
-        })
-      }
-
-      this.setState(function () {
-        return {
-          error: null,
-          winner: results[0],
-          looser: results[1],
+        return this.setState(() => ({
+          error: 'Looks like there was an error. Check that both users exist on Github',
           loading: false
         }
-      })
-    }.bind(this))
+        ))
+      }
+
+      this.setState(() => ({
+        error: null,
+        winner: results[0],
+        looser: results[1],
+        loading: false
+      }
+      ))
+    })
   }
 
   render() {
-    var error = this.state.error
-    var winner = this.state.winner
-    var loser = this.state.looser
-    var loading = this.state.loading
+    const { error, winner, loser, loading } = this.state;
 
     if (loading === true) {
       return <Loading />
